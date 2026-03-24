ef239920bb00367efbf26b7c3033de53752ae3e0
armylong <armylong@163.com>
2026-03-24 13:48:30 +0800

    image_processor

--- internal/cmd/image_processor.py
+++ internal/cmd/image_processor.py
@@ -1,5 +1,5 @@
 import click
-from business.image_processor import ImageBatchProcessor
+from internal.business.image_processor import ImageBatchProcessor
 
 @click.command(name='image_processor')
 @click.option('--input', '-i', type=str, default='', help='输入目录路径')
@@ -11,4 +11,4 @@ def image_processor_handler(input, output, type, width, height):
     """图片批量处理"""
     processor = ImageBatchProcessor(input_dir=input, output_dir=output)
     processor.process(process_type=type, resize_w=width, resize_h=height)
-    return processor.stats()
+    return processor.stats
