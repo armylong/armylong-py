76f2e5135ff57a66a1bf5cef66e49e977766a3c9
armylong <armylong@163.com>
2026-04-01 15:49:16 +0800

    logging

--- internal/cmd/coding_agent.py
+++ internal/cmd/coding_agent.py
@@ -7,9 +7,8 @@ from internal.business.coding_agent.main import Main
 @click.option('--record_ids', type=str, default='', help='记录ID列表（可选，逗号分隔）')
 def coding_agent_handler(action, record_ids):   
     """coding_agent命令核心逻辑：仅读取参数 + 处理业务"""
-    logging.info("coding_agent命令核心逻辑")
-    logging.info(f"action: {action}")
-    logging.info(f"record_ids: {record_ids}")
+    print()
+    logging.info(f"coding_agent {action} --record_ids: {record_ids}")
     if action == "":
         logging.error("请输入任务类型")
         return
@@ -21,10 +20,8 @@ def coding_agent_handler(action, record_ids):
         record_ids = []
 
     if action == "save_works":
-        logging.info("save_works")
         main.save_works(record_ids=record_ids)
     elif action == "update_feishu_doc":
-        logging.info("update_feishu_doc")
         main.update_feishu_doc(record_ids=record_ids)
     else:
         logging.error(f"不支持的任务类型: {action}")
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import logging
 logging.basicConfig(
     level=logging.INFO,          # 允许输出的最小日志级别 DEBUG INFO WARNING ERROR CRITICAL FATAL
-    format="%(asctime)s [%(levelname)s] %(filename)s:%(funcName)s():%(lineno)d - %(message)s",  # 日志格式 #  %(asctime)s 时间  %(levelname)s 日志级别（INFO/WARNING/ERROR） %(message)s 你写的日志内容  %(filename)s 文件名（例如 main.py）  %(lineno)d 行号  %(funcName)s 函数名  %(module)s 模块名  %(process)d 进程ID  %(thread)d 线程ID  %(name)s logger名字
+    format="%(asctime)s [%(levelname)s] - %(message)s\t# %(filename)s.%(funcName)s():%(lineno)d",  # 日志格式 #  %(asctime)s 时间  %(levelname)s 日志级别（INFO/WARNING/ERROR） %(message)s 你写的日志内容  %(filename)s 文件名（例如 main.py）  %(lineno)d 行号  %(funcName)s 函数名  %(module)s 模块名  %(process)d 进程ID  %(thread)d 线程ID  %(name)s logger名字
     datefmt="%Y-%m-%d %H:%M:%S",               # 时间格式
     stream=sys.stderr,                 # 输出到控制台 sys.stderr(默认) sys.stdout（和 filename 二选一）
     # filename="app.log",          # 输出到文件 app.log （和 stream 二选一）
